2dc358ff9310aa0569bbaa3ca9fb4fc0b96fd016
MForever78 <shunjian1128@gmail.com>
2014-06-16 16:31:06 +0800

    eliminates rubbish

--- P.c
+++ P.c
@@ -4,28 +4,22 @@ void draw_interface(int interface)
 {
 	switch (interface) {
 		case 0:	//welcome interface
-			// set_palette(game_palette[AWEL]);
 			draw_picture(pic[AWEL], 0, 0);
 			break;
 		case 1: //game interface
 			draw_picture(pic[BGAME], 0, 0);
-			// set_palette(game_palette[BMOUNT]);
-			// draw_picture(pic[BMOUNT], 0, 0);
+			draw_picture(pic[BMOUNT], 0, 0);
 			break;
 		case 2: //help interface
-			// set_palette(game_palette[HELP]);
 			draw_picture(pic[HELP], 0, 0);
 			break;
 		case 3: //pause interface
-			// set_palette(game_palette[CPAUSE]);
 			draw_picture(pic[CPAUSE], 0, 0);
 			break;
 		case 4: //WIN1 interface
-			// set_palette(game_palette[DWIN1]);
 			draw_picture(pic[DWIN1], 0, 0);
 			break;
 		case 5: //WIN2 interface
-			// set_palette(game_palette[DWIN2]);
 			draw_picture(pic[DWIN2], 0, 0);
 			break;
 	}
@@ -37,18 +31,14 @@ void draw_player(int index, int moving)
 	if (index == 0) {
 		if (moving) {
 			worm = pic[WORM01];
-			// set_palette(game_palette[WORM01]);
 		} else {
 			worm = pic[WORM00];
-			// set_palette(game_palette[WORM00]);
 		}
 	} else {
 		if (moving) {
 			worm = pic[WORM11];
-			// set_palette(game_palette[WORM11]);
 		} else {
 			worm = pic[WORM10];
-			// set_palette(game_palette[WORM10]);
 		}
 	}
 	putimage(player[index].x - worm.mask->picwidth / 2, player[index].y - worm.mask->picheight, worm.mask, AND_PUT);
@@ -57,12 +47,10 @@ void draw_player(int index, int moving)
 
 void draw_bullet()
 {
-	// set_palette(game_palette[BULLET0]);
 	draw_picture(pic[BULLET0], bullet.x, bullet.y);
 }
 
 void draw_explode()
 {
-	// set_palette(game_palette[EXPLO0 + explode_state]);
 	draw_picture(pic[EXPLO0 + explode_state], bullet.x, bullet.y);	
 }
